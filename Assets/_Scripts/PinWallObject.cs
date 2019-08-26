aa34a869d5098185646d806fe020cf6e8c51e966
EtowTheOffieCat <mabonp@gmail.com>
2019-08-26 14:15:54 +0200

    commit 26.08.19

--- Assets/_Scripts/PinWallObject.cs
+++ Assets/_Scripts/PinWallObject.cs
@@ -6,8 +6,11 @@ public class PinWallObject : Interactable
 {
     
     public GameObject Point1;
+    public string Positiontext;
     private Quaternion rotation1;
+    private Quaternion thisObjectRotation;
     private Vector3 Vector1;
+
 
     private void Start()
     {
@@ -15,9 +18,14 @@ public class PinWallObject : Interactable
         rotation1 = Point1.transform.rotation;
 
     }
+
+    private void Update()
+    {
+        thisObjectRotation = this.transform.rotation;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("PinWall")) 
+        if (other.CompareTag(Positiontext) ) 
         {
             this.transform.position = Vector1;
             this.transform.rotation = rotation1;
